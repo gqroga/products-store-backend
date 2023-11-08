fdda888e2568a0df261394676d3ec7bcd13c0d0a
gqroga <gqueirogat@gmail.com>
2023-11-08 11:42:09 -0300

    first commit

---
+++ src/main/java/com/productsstore/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.productsstore.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.productsstore.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/test/java/com/productsstore/demo/ProductsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productsstore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
